b266e8eae41caad25c417a3093a28539143899b2
Raquel Marie <raqueldelamer@gmail.com>
2024-12-01 23:29:02 -0600

    complete destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -32,22 +32,50 @@ console.log(language); // Output: English
 // 1. Destructure the following array to get the first, second, and third elements
 // const fruits = ["apple", "banana", "cherry", "date"];
 // // Your code here
+const fruits = ["apple", "banana", "cherry", "date"];
+const [firstFruit, secondFruit, thirdFruit] = fruits;
+console.log(firstFruit);  //output = apple
+console.log(secondFruit); //output = banana
+console.log(thirdFruit);  //output = cherry
 
 // 2. Destructure the following object to extract the title and author properties
 // const book = { title: "1984", author: "George Orwell", year: 1949 };
 // // Your code here
+const book = { title: "1984", author: "George Orwell", year: 1949 };
+const { title, author, year } = book;
+console.log(title);    // output= 1984
+console.log(author);   // output= George Orwell
+console.log(year);     // output = 1949
 
 // 3. Use destructuring to swap the values of two variables
 // let a = 1;
 // let b = 2;
 // // Your code here
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+
+console.log(a);  // output= 2
+console.log(b);  // output= 1
 
 // 4. Destructure the following object to extract the name and city properties
 // const person = { name: "John", city: "New York", zip: "10001" };
 // // Your code here
+const person = { firstName: "John", city: "New York", zip: "10001" };
+const { firstName, city, zip } = person;
+console.log(firstName);     // output= John
+console.log(city);          // output= New York
+console.log(zip);           // output= 10001
+
 
 // 5. Destructure an array and use the rest operator to collect the remaining elements
 // const numbers = [10, 20, 30, 40, 50];
 // // Your code here
+const numbers = [10, 20, 30, 40, 50];
+const [first, second, ...rest] = numbers;
+
+console.log(first);  // output= 10
+console.log(second); // output= 20
+console.log(rest);   // output= [30, 40, 50]
 
 // Solutions should be written directly below each TODO comment.
